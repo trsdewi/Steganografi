846f759fab23458983734df80bcf7563b9b2ab4c
trsdewi <76573977+trsdewi@users.noreply.github.com>
2023-12-31 23:12:51 +0800

    Add files via upload

---
+++ 2008561004/UAS/bpcs/encode.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser
+
+from PIL import Image
+
+import numpy as np
+import bpcs  as bp
+
+parser = ArgumentParser()
+parser.add_argument("-i", "--image",  type=str, help="dummy image path", required=True)
+parser.add_argument("-s", "--secret", type=str, help="secret message text file path", required=True)
+parser.add_argument("-o", "--output", type=str, help="output image path", required=True)
+args = parser.parse_args()
+
+blocksize = (8,8)
+ath       = 0.45 # complexity threshold
+
+# prepare secret blocks
+arr = bp.read_message_as_numpy(args.secret, blocksize)
+arr = bp.to_binary(arr)
+secret_blocks, conj_map = bp.secret_blocks(arr, blocksize, ath)
+
+# encode
+arr = bp.read_image_as_numpy(args.image)
+arr = bp.to_binary(arr)
+arr = bp.encode(arr, secret_blocks, conj_map, blocksize, ath)
+
+if arr is not None:
+    arr = bp.to_image(arr)
+    Image.fromarray(np.uint8(arr)).show()
+    Image.fromarray(np.uint8(arr)).save(args.output)
+else:
+    print("Invalid dummy image size or secret file size!")
